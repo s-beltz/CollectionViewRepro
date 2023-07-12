69db0ff1855048540df16b69829fbc2e6a4ea73a
s-beltz <57802605+s-beltz@users.noreply.github.com>
2023-07-12 16:40:16 +0200

    Add project files.

---
+++ MauiCollectionViewApp/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace MauiCollectionViewApp
+{
+    public partial class MainPage : ContentPage
+    {
+        #region Tips
+
+        public List<string> Tiles
+        {
+            get { return (List<string>)GetValue(TilesProperty); }
+            set { SetValue(TilesProperty, value); }
+        }
+
+        public static readonly BindableProperty TilesProperty = BindableProperty.Create(
+            nameof(Tiles),
+            typeof(List<string>),
+            typeof(MainPage),
+            defaultValue: null, defaultBindingMode: BindingMode.OneWay);
+
+        #endregion Tips
+
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Tiles = new List<string> { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", };
+        }
+
+
+    }
+}
